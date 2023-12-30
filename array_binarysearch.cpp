71f29c9d4f7b82dc335da4e4f4b6141589d5860a
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2023-12-30 21:12:42 +0530

    Add files via upload

---
+++ array_binarysearch.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+    int binary_search(int k,int array[],int key)
+
+    {
+        int s=0;
+        int e=k;
+        while(s<=e){
+                int mid=(s+e)/2;
+
+                if(array[mid]==key){
+                    return mid+1;
+                }
+
+            else if(array[mid]>key)
+
+                {e=mid-1;}
+            else{s=mid+1;}
+        }
+    return -1;
+    }
+int main()
+{
+   int k;
+   cin>>k;
+
+
+     int array[k];
+     for( int i=0;i<k;i++){
+        cin>>array[i];
+
+     }
+
+    int key;
+    cin>>key;
+
+    cout<<binary_search(k,array,key)<<endl;
+
+}
