92949118ad898439208a6e6eab6ac2b18a71c3cc
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-01 13:09:01 +0530

    Add files via upload

---
+++ insert_fixed.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int insert(int n, int arr[], int x, int pos) {
+    if (pos < 0 || pos > n) {
+        cout << "Invalid position for insertion." << endl;
+        return n; // Return the original size of the array
+    }
+
+    for (int i = n - 1; i >= pos; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[pos] = x;
+    return (n + 1);
+}
+
+int main() {
+    int arr[15] = {2, 4, 1, 8, 5};
+    int n = 5;
+
+    cout << "\n Before insertion: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " " << endl;
+
+    cout << "element to be inserted = ";
+    int x;
+    cin >> x;
+
+    int pos;
+    cout << "enter the position to insert element= ";
+    cin >> pos;
+
+    n = insert(n, arr, x, pos);
+
+    cout << "\n After Insertion: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " " << endl;
+
+    return 0;
+}
+
