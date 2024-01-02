8c955ce527f5e66c96f4e00742f4d5cc7c243f53
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-02 11:09:33 +0530

    Add files via upload

---
+++ EQLZING.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int v,b;
+	    cin>>v>>b;
+	    if(abs(v-b)%2==0)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+
+}
+
+
