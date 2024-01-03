14cd6977e0482312a9c43b1d55babf6c0bec086b
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-03 14:50:42 +0530

    Add files via upload

---
+++ count_digit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int t;
+ 	cout<<"ENTER NUMBER OF TEST CASES = ";
+ 	cin>>t;
+ 	while(t--)
+ 	{
+ 		int n;
+ 	    cout<<"Enter a Number = ";
+ 	    cin>>n;
+ 	    int res=0;
+ 	    while(n>0)
+ 	     {
+ 		    n=n/10;
+ 		    res++;
+	      }
+	      cout<<res<<endl;
+	 }
+	 return 0;
+ }
