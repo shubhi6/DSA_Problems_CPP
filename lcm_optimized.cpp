aa19113a39843a599fe56886f5c8404ef896a5ab
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-03 14:46:39 +0530

    Add files via upload

---
+++ lcm_optimized.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int GCD(int n,int m)
+{
+		if(m==0)
+		return n;
+		else
+		return GCD(m,n%m);
+}
+
+int LCM(int n,int m)
+{
+	return (n*m)/GCD(n,m);
+	
+}
+
+int main()
+{
+    int t;
+    cout<<"Number of Test Cases = ";
+    cin>>t;
+    while(t--)
+    {
+    		int n,m;
+	        cout<<"Enter Two Number = ";
+           	cin>>n>>m;
+          	cout<<"LCM of Numbers = " <<LCM(n,m)<<endl;
+	}
+	return 0;
+}
