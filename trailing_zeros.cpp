54dc1839e9b8615d0f01cdcea12713837bfd8e65
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-03 14:39:44 +0530

    Add files via upload

---
+++ trailing_zeros.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int trailing_of_zeros(int n)
+{
+	int res=0;
+	for(int i=5;i<=n;i=i*5)
+    res=res+(n/i);
+	return res;
+	
+}
+
+int main()
+{
+    int t;
+    cout<<"Number of Test Cases = ";
+    cin>>t;
+    while(t--)
+    {
+    		int n;
+	        cout<<"Enter a Number = ";
+           	cin>>n;
+          	cout<<"ZEROS = " <<trailing_of_zeros(n)<<endl;
+	}
+	return 0;
+}
