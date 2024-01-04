e1add597b85fb191e9603a5e89e3384b8d5ab239
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-04 12:42:38 +0530

    Add files via upload

---
+++ LCM.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int LCM(int n,int m)
+{
+	int res= max(m,n);
+	while(true)
+	{
+		if(res%n==0 && res%m==0)
+		return res;
+        res++;
+	}
+	return res;
+	
+}
+
+int main()
+{
+    int t;
+    cout<<"Number of Test Cases = ";
+    cin>>t;
+    while(t--)
+    {
+    		int n,m;
+	        cout<<"Enter Two Number = ";
+           	cin>>n>>m;
+          	cout<<"LCM of Numbers = " <<LCM(n,m)<<endl;
+	}
+	return 0;
+}
