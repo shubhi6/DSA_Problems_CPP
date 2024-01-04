e9162a7afa092f3b467e9a07aa30f320ed599d0e
shubhi6 <155247726+shubhi6@users.noreply.github.com>
2024-01-04 12:39:27 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n)
+{
+	if(n==0||n==1)
+	return n;
+	int a=0; int b=1,c;
+	for(int i=2;i<=n;i++)
+	{
+		c=a+b;
+		a=b;
+		b=c;	
+	}
+	return c;
+}
+
+int main()
+{
+	int t;
+	cout<<"ENTER NUMBER OF TEST CASES = ";
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cout<<"\n Enter a number = ";
+		cin>>n;
+		cout<<fib(n)<<endl;
+	}
+	return 0;
+}
