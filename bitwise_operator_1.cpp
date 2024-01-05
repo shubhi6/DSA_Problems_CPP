c309f412e17f09ef24a93943b1c1d96828077ffb
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-05 19:31:08 +0530

    Add files via upload

---
+++ bitwise_operator_1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+     // bitwise AND operator
+     
+     
+int main()
+{
+	int x=3; int y=6;
+	cout<<(x&y);
+	return 0;
+}
+    // bitwise OR operator
+    
+    
+int main()
+{
+	int x=3; int y=6;
+	cout<<(x|y);
+	return 0;
+}
+     // bitwise XOR operator
+     
+     
+int main()
+{
+	int x=3; int y=6;
+	cout<<(x^y);
+	return 0;
+}
