cec8bd090e45081ea7c70307623edc9a43daa66c
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-05 19:31:27 +0530

    Add files via upload

---
+++ bitwiser_operator_2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+    // bitwise LEFT SHIFT operator
+
+//int main()
+//{
+//	int x=3; int y=4;
+//	cout<<(x<<1)<<endl;
+//	cout<<(x<<2)<<endl;
+//	
+//	int z=(x<<y);
+//	cout<<(x<<y)<<endl;
+//}
+
+   // bitwise RIGHT SHIFT operator
+
+int main()
+{
+	int x=33; int y=4;
+	cout<<(x>>1)<<endl;
+	cout<<(x>>2)<<endl;
+	
+	int z=(x>>y);
+	cout<<(x>>y)<<endl;
+}
