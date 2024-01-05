4210be74aef9819c95954c08544a431b60893f72
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-05 19:33:40 +0530

    Update bitwise_operator_3.cpp

--- bitwise_operator_3.cpp
+++ bitwise_operator_3.cpp
@@ -1,18 +1,20 @@
+// implementation of bitwise operator in 2 parts
+
 #include<iostream>
 using namespace std;
 
     // bitwise NOT operator   (UNSIGNED)
 
-//int main()
-//{
-//	unsigned int x=1;
-//	cout<<(~x)<<endl;
-//	 x=5;
-//	cout<<(~x)<<endl;
-//}
+int main()
+{
+	unsigned int x=1;
+	cout<<(~x)<<endl;
+	 x=5;
+	cout<<(~x)<<endl;
+}
 
 
-    // bitwise NOT operator   (SIGNED)
+    bitwise NOT operator   (SIGNED)
 
 int main()
 {
