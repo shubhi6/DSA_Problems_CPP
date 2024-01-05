f41d45a7e55a81dd9d048cd0bf8c0c1098ee31dc
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-05 19:31:40 +0530

    Add files via upload

---
+++ bitwise_operator_3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+    // bitwise NOT operator   (UNSIGNED)
+
+//int main()
+//{
+//	unsigned int x=1;
+//	cout<<(~x)<<endl;
+//	 x=5;
+//	cout<<(~x)<<endl;
+//}
+
+
+    // bitwise NOT operator   (SIGNED)
+
+int main()
+{
+	int x=1;
+	cout<<(~x)<<endl;
+	 x=5;
+	cout<<(~x)<<endl;
+}
+
