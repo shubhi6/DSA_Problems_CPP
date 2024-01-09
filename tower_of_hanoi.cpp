f07413871cf84bb26b5dc95c8138d1d59a4d9ded
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-09 19:20:47 +0530

    Update tower_of_hanoi.cpp

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -1,3 +1,13 @@
+//You are given three pegs (towers i.e, A,B,C) and a number of disks of different sizes, which can slide onto any peg.
+//The puzzle starts with the disks in a neat stack in ascending order of size on one peg, the smallest at the top, thus making a conical shape. 
+
+//The objective of the puzzle is to move the entire stack to another peg, obeying the following simple rules:
+// 1. Only one disk can be moved at a time.
+// 2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty peg.
+// 3. No disk may be placed on top of a smaller disk.
+
+
+
 #include<iostream>
 using namespace std;
 
