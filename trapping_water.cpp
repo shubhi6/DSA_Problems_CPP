0e5b46b43d69ec66dd3d8f55fc3704058dcdebc0
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-12 19:42:28 +0530

    Update trapping_water.cpp

--- trapping_water.cpp
+++ trapping_water.cpp
@@ -1,3 +1,30 @@
+// Taking an array of N non-negative integers arr[] representing an elevation map where the width of each bar is 1, 
+// compute how much water it is able to trap after raining.
+
+// Examples:  
+
+// Input: arr[] = {2, 0, 2}
+// Output: 2
+// Explanation: The structure is like below.
+// We can trap 2 units of water in the middle gap.
+
+
+
+// Input: arr[]   = {3, 0, 2, 0, 4}
+// Output: 7
+// Explanation: Structure is like below.
+// We can trap “3 units” of water between 3 and 2,
+// “1 unit” on top of bar 2 and “3 units” between 2 and 4.
+
+
+
+// Input: arr[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+// Output: 6
+// Explanation: The structure is like below.
+// Trap “1 unit” between first 1 and 2, “4 units” between
+// first 2 and 3 and “1 unit” between second last 1 and last 2
+
+	
 #include<iostream>
 #include<cmath>
 using namespace std;
