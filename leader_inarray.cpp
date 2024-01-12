3f21ce5b69077ea5f4fd6274019851430e2cd584
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-12 12:59:45 +0530

    Update leader_inarray.cpp

--- leader_inarray.cpp
+++ leader_inarray.cpp
@@ -1,3 +1,14 @@
+//Write a program to print all the LEADERS in the array. 
+//An element is a leader if it is greater than all the elements to its right side. And the rightmost element is always a leader. 
+
+//For example:
+
+//Input: arr[] = {16, 17, 4, 3, 5, 2}, 
+//Output: 17, 5, 2
+
+//Input: arr[] = {1, 2, 3, 4, 5, 2}, 
+//Output: 5, 2
+
 #include<iostream>
 #include<cmath>
 using namespace std;
