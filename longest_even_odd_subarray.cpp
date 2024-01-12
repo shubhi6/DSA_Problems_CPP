06d8c93086f20448764e0abd4f4dfa7e1d988e94
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-12 19:40:09 +0530

    Update longest_even_odd_subarray.cpp

--- longest_even_odd_subarray.cpp
+++ longest_even_odd_subarray.cpp
@@ -1,3 +1,17 @@
+//Through this code we can find the length of the longest Alternating Even Odd subarray present in the array. 
+
+//Examples: 
+
+//Input: a[] = {1, 2, 3, 4, 5, 7, 9} 
+//Output: 5 
+//Explanation: 
+//The subarray {1, 2, 3, 4, 5} has alternating even and odd elements.
+
+//Input: a[] = {1, 3, 5} 
+//Output: 0 
+//Explanation: 
+//There is no such alternating sequence possible. 
+
 #include<iostream>
 #include<cmath>
 using namespace std;
