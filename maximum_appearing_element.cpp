5648c6421cbbd651c9c3107c4f6a312e01807fba
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-13 20:33:12 +0530

    Update maximum_appearing_element.cpp

--- maximum_appearing_element.cpp
+++ maximum_appearing_element.cpp
@@ -1,3 +1,21 @@
+// Given two arrays L[ ] and R[ ] of size N where L[i] and R[i] (0 ≤ L[i], R[i] < 106) denotes a range of numbers, the task is to find the maximum occurred integer in all the ranges. If more than one such integer exists, print the smallest one. 
+// Through this we can find the maximum appearing elements.
+
+// Thus 4 is the Most appeared Element in the Ranges
+// Examples: 
+
+// Input: L[ ] = {1, 4, 3, 1}, R[ ] = {15, 8, 5, 4}
+// Output: 4
+
+// Explanation: Overall ranges are: {1,2,3,4,5,6,7,8,9,10,11,12,13,14 15}, {4,5,6,7,8}, {3,4,5}, {1,2,3,4}. 
+
+// In all these ranges, 4 appears the most times.
+
+// Input: L[ ] = {1, 5, 9, 13, 21}, R[ ] = {15, 8, 12, 20, 24}
+// Output: 5
+// Explanation: Overall Ranges are: {1,2,3,4,5,6,7,8,9,10,11,12,13,14 15}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16,17,18,19,20},{21,22,23,24}
+// In these ranges, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 all appear 2 times. The smallest number among all are 5.
+
 #include <iostream>
 using namespace std;
 
