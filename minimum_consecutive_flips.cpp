ae25a09f82f3a01f2210cf87c3749ddcb66eaae7
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-13 12:47:28 +0530

    Update minimum_consecutive_flips.cpp

--- minimum_consecutive_flips.cpp
+++ minimum_consecutive_flips.cpp
@@ -1,9 +1,12 @@
-// Given a binary array, we need to convert this array into an array that either contains all 1s or all 0s.  We need to do it using the minimum number of group flips. 
+// Given a binary array, we need to convert this array into an array that either contains all 1s or all 0s. 
+//We need to do it using the minimum number of group flips. 
 // Examples : 
 
 // Input : arr[] = {1, 1, 0, 0, 0, 1}
 // Output :  From 2 to 4
-// Explanation : We have two choices, we make all 0s or do all 1s.  We need to do two group flips to make all elements 0 and one group flip to make all elements 1.  Since making all elements 1 takes the least group flips, we do this.
+// Explanation : We have two choices, we make all 0s or do all 1s. 
+//We need to do two group flips to make all elements 0 and one group flip to make all elements 1.  
+//Since making all elements 1 takes the least group flips, we do this.
  
 
 // Input : arr[] = {1, 0, 0, 0, 1, 0, 0, 1, 0, 1}
