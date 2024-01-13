0ca7f579c47c1967bddc8cc46478ca7a2de050a3
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-13 12:45:31 +0530

    Update majority_element.cpp

--- majority_element.cpp
+++ majority_element.cpp
@@ -1,3 +1,17 @@
+//A majority element in an array A[] of size n is an element that appears more than n/2 times (and hence there is at most one such element). 
+
+//Examples : 
+
+//Input : {3, 3, 4, 2, 4, 4, 2, 4, 4}
+//Output : Majority element is = 4
+//         Majority element index of an array is = 4
+//Explanation: The frequency of 4 is 5 which is greater than the half of the size of the array size. 
+
+//Input : {3, 3, 4, 2, 4, 4, 2, 4}
+//Output : No Majority Element
+//Explanation: There is no element whose frequency is greater than the half of the size of the array size.
+
+
 #include<iostream>
 using namespace std;
 
@@ -24,7 +38,7 @@ int majority_element(int arr[],int n)
 		if(count<=n/2)
 		res=-1;
 		cout<<"Majority element is = "<<arr[res]<<endl;
-			cout<<"Majority element index of an array is = ";
+		cout<<"Majority element index of an array is = ";
 	  return res;
 	
 	
