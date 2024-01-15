1bb0c3c484bf00ed30cb395924380644459b6d7a
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-15 20:10:21 +0530

    Update search_insorted_rotated_array.cpp

--- search_insorted_rotated_array.cpp
+++ search_insorted_rotated_array.cpp
@@ -1,3 +1,12 @@
+// Through this code we are given a sorted array that is rotated a certain number of times . 
+// Our task is to detect the presence of a given target element in the array
+// EXAMPLE:
+// Input- arr[]={10,20,30,40,50,8,9} x=30
+// Output- 2 // index of the element.
+
+// Input- arr[]={100,200,300,10,20} x=40
+// Output- -1 // this shows element is not present.
+
 #include<iostream>
 using namespace std;
 
