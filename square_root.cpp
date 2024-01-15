1267128a206538b2c8e86e71fc736e9de166fb04
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-15 20:03:40 +0530

    Update square_root.cpp

--- square_root.cpp
+++ square_root.cpp
@@ -1,3 +1,15 @@
+// Given an integer X, find its square root. If X is not a perfect square, then return floor(√x).
+
+// Examples : 
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2.
+
+// Input: x = 11
+// Output: 3
+// Explanation:  The square root of 11 lies in between 3 and 4 so floor of the square root is 3.
+
 #include<iostream>
 using namespace std;
 
