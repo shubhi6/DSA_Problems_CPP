6d35ec6d5ee1dc46f36478c675195847c5224333
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-16 20:37:31 +0530

    Update repeating_element.cpp

--- repeating_element.cpp
+++ repeating_element.cpp
@@ -1,3 +1,22 @@
+// Through this code we can find the repeating element in the array, there are certain rules which should be taken care of :-
+// 1. O(n) should be the time complexity.
+// 2. O(1) should be the space.
+// 3. No modifications can be done to the original array.
+
+// * Array size should be greater than 2
+// * Only one elements repeats (any number of times).
+// * All the elements from 0 to max(arr) are presnet.
+
+// Therefore 0 <= max(arr) <= n-2.
+
+// EXAMPLE:
+// INPUT: Arr[] : { 0,2,1,3,2,2}
+// OUTPUT: Repeating element = 2
+
+// INPUT: Arr[] : { 1,2,3,0,3,4,5}
+// OUTPUT: Repeating element = 3
+
+
 #include<iostream>
 using namespace std;
 
