8f052c63987930a213a3aa87b04b10a4b5638e7d
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-16 20:28:20 +0530

    Update allocate_minimum_pages.cpp

--- allocate_minimum_pages.cpp
+++ allocate_minimum_pages.cpp
@@ -1,3 +1,20 @@
+// Given a number of pages in N different books and K students. The books are arranged in ascending order of the number of pages.
+// Every student is assigned to read some consecutive books.
+// The task is to assign books in such a way that the maximum number of pages assigned to a student is minimum. 
+
+// Example : 
+
+// Input : pages[] = {12, 34, 67, 90} , K = 2
+
+// Output : Minimum number of pages = 113
+
+// Explanation: There are 2 number of students. Books can be distributed in following fashion : 
+// 1) [12] and [34, 67, 90]
+// Max number of pages is allocated to student ‘2’ with 34 + 67 + 90 = 191 pages
+// 2) [12, 34] and [67, 90] Max number of pages is allocated to student ‘2’ with 67 + 90 = 157 pages 
+// 3) [12, 34, 67] and [90] Max number of pages is allocated to student ‘1’ with 12 + 34 + 67 = 113 pages
+// Of the 3 cases, Option 3 has the minimum pages = 113.
+
 #include<iostream>
 using namespace std;
 
