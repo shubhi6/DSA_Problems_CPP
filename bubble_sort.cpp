ebb692a9ffa8d9e2e7781acbd133ebe1d781abdc
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-17 13:29:31 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,4 +1,7 @@
- #include<iostream>
+// Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order.
+// This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high.
+
+#include<iostream>
 using namespace std;
 
 int bubblesort(int arr[],int n)
@@ -32,7 +35,35 @@ int main()
     bubblesort(arr,n);
     printArray(arr, n);
 }
+
  
+
+// How Bubble Sort Works?
+ 
+// Consider an array arr[] = {5, 1, 4, 2, 8}
+
+// First Pass: 
+
+// Bubble sort starts with very first two elements, comparing them to check which one is greater.
+// ( 5 1 4 2 8 ) --> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1. 
+// ( 1 5 4 2 8 ) -->  ( 1 4 5 2 8 ), Swap since 5 > 4 
+// ( 1 4 5 2 8 ) -->  ( 1 4 2 5 8 ), Swap since 5 > 2 
+// ( 1 4 2 5 8 ) --> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+// Second Pass: 
+
+// Now, during second iteration it should look like this:
+// ( 1 4 2 5 8 ) --> ( 1 4 2 5 8 ) 
+// ( 1 4 2 5 8 ) --> ( 1 2 4 5 8 ), Swap since 4 > 2 
+// ( 1 2 4 5 8 ) --> ( 1 2 4 5 8 ) 
+// ( 1 2 4 5 8 ) -->  ( 1 2 4 5 8 ) 
+// Third Pass: 
+
+// Now, the array is already sorted, but our algorithm does not know if it is completed.
+// The algorithm needs one whole pass without any swap to know it is sorted.
+// ( 1 2 4 5 8 ) --> ( 1 2 4 5 8 ) 
+// ( 1 2 4 5 8 ) --> ( 1 2 4 5 8 ) 
+// ( 1 2 4 5 8 ) --> ( 1 2 4 5 8 ) 
+// ( 1 2 4 5 8 ) --> ( 1 2 4 5 8 ) 
  
  
  
