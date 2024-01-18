61d736b6d14eee31c8a1dd2d20b68ac9cec49f47
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-18 12:59:40 +0530

    Update count_inversions_inarray.cpp

--- count_inversions_inarray.cpp
+++ count_inversions_inarray.cpp
@@ -1,3 +1,20 @@
+// Inversion Count for an array indicates – how far (or close) the array is from being sorted.
+// If the array is already sorted, then the inversion count is 0, but if the array is sorted in reverse order, the inversion count is the maximum. 
+
+// Given an array a[]. The task is to find the inversion count of a[]. Where two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+
+// Examples: 
+
+// Input: arr[] = {8, 4, 2, 1}
+// Output: 6
+// Explanation: Given array has six inversions: (8, 4), (4, 2), (8, 2), (8, 1), (4, 1), (2, 1).
+
+ 
+
+// Input: arr[] = {1, 20, 6, 4, 5}
+// Output: 5
+// Explanation: Given array has five inversions: (20, 6), (20, 4), (20, 5), (6, 4), (6, 5). 
+
 #include <bits/stdc++.h>
 using namespace std;
 
