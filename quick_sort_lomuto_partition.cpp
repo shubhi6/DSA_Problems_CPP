5e870a249ca10460a1043fde071feb1d17fab30a
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-18 19:47:36 +0530

    Add files via upload

---
+++ quick_sort_lomuto_partition.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout<<arr[i]<<" ";
+    cout<<"\n";
+}
+ 
+// Driver program to test above functions
+int main()
+{
+     int j, n;
+    cout << "Enter size of array = ";
+    cin >> n;
+    cout << "Enter elements of array = ";
+    int arr[n];
+    for (j = 0; j < n; j++)
+    cin >> arr[j];
+    quickSort(arr, 0, n-1);
+    cout<<"Sorted array: "<<"\n";
+    printArray(arr, n);
+    return 0;
+}
