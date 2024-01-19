18f566d357ac89b6bf606938206c1ed1d865b71f
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-19 19:49:19 +0530

    Update kth_smallest_element.cpp

--- kth_smallest_element.cpp
+++ kth_smallest_element.cpp
@@ -1,3 +1,13 @@
+// Given an array and a number k where k is smaller than the size of the array, we need to find the k'th smallest element in the given array. It is given that all array elements are distinct.
+
+// Examples:  
+
+// Input: arr[] = {7, 10, 4, 3, 20, 15}, k = 3 
+// Output: K'th smallest element is  = 7 
+
+// Input: arr[] = {7, 10, 4, 3, 20, 15}, k = 4 
+// Output: K'th smallest element is = 10 
+
 #include <algorithm>
 #include <iostream>
 using namespace std;
