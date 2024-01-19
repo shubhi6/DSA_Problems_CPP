f9b15baa5162392518a1a95a240e48541dfa6f0f
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-19 19:50:33 +0530

    Update minimum_difference_inan_array.cpp

--- minimum_difference_inan_array.cpp
+++ minimum_difference_inan_array.cpp
@@ -1,3 +1,21 @@
+// Find minimum difference between any two elements (pair) in given array
+
+// Given an unsorted array, find the minimum difference between any pair in the given array.
+
+// Examples :
+
+// Input: {1, 5, 3, 19, 18, 25}
+// Output: Minimum difference in an array = 1
+// Explanation: Minimum difference is between 18 and 19
+
+// Input: {30, 5, 20, 9}
+// Output:Minimum difference in an array =  4
+// Explanation: Minimum difference is between 5 and 9
+
+// Input: {1, 19, -4, 31, 38, 25, 100}
+// Output: Minimum difference in an array = 5
+// Explanation: Minimum difference is between 1 and -4
+
 #include <bits/stdc++.h>
 using namespace std;
 
