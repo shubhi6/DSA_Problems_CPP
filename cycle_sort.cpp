ed8abdf1b3663c42959cfb12b945162ea19c208b
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-20 18:42:38 +0530

    Update cycle_sort.cpp

--- cycle_sort.cpp
+++ cycle_sort.cpp
@@ -1,3 +1,14 @@
+// Cycle sort is an in-place sorting Algorithm, unstable sorting algorithm, 
+// and a comparison sort that is theoretically optimal in terms of the total number of writes to the original array. 
+ 
+
+// It is optimal in terms of the number of memory writes. It minimizes the number of memory writes to sort 
+// (Each value is either written zero times if it’s already in its correct position or written one time to its correct position.)
+// It is based on the idea that the array to be sorted can be divided into cycles. Cycles can be visualized as a graph.
+//  We have n nodes and an edge directed from node i to node j if the element at i-th index must be present at j-th index in the sorted array. 
+// Cycle in arr[] = {2, 4, 5, 1, 3} 4
+//  Cycle in arr[] = {4, 3, 2, 1}
+
 #include <iostream>
 using namespace std;
  
