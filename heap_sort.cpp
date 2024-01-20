e477a396abcf670c6b1dcba90c68ca85bed75394
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-20 18:41:20 +0530

    Update heap_sort.cpp

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -1,3 +1,34 @@
+// Heap sort is a comparison based sorting technique based on Binary Heap data structure.
+// It is similar to selection sort where we first find the maximum element and place the maximum element at the end. 
+// We repeat the same process for remaining elements.
+// Input data: [4, 10, 3, 5, 1]
+//          4(0)
+//         /   \
+//      10(1)   3(2)
+//     /   \
+//  5(3)    1(4)
+
+// The numbers in bracket represent the indices in the array 
+// representation of data.
+
+// Applying heapify procedure to index 1:
+//          4(0)
+//         /   \
+//     10(1)    3(2)
+//     /   \
+// 5(3)    1(4)
+
+// Applying heapify procedure to index 0:
+//         10(0)
+//         /  \
+//      5(1)  3(2)
+//     /   \
+//  4(3)    1(4)
+
+// The heapify procedure calls itself recursively to build heap
+// in top down manner.
+
+
 #include <iostream>
 using namespace std;
 
