22be8fd66033344c9ed5b1c272598820b9b67021
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-20 18:39:23 +0530

    Update radix_sort.cpp

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,3 +1,28 @@
+// The lower bound for the Comparison based sorting algorithm (Merge Sort, Heap Sort, Quick-Sort .. etc) is Ω(nLogn), i.e.,
+//     they cannot do better than nLogn. Counting sort is a linear time sorting algorithm that sort in O(n+k) time when elements are in the range from 1 to k.
+
+// What if the elements are in the range from 1 to n2? 
+
+ 
+
+// We can’t use counting sort because counting sort will take O(n2) which is worse than comparison-based sorting algorithms. Can we sort such an array in linear time? 
+
+// Radix Sort is the answer. The idea of Radix Sort is to do digit by digit sort starting from least significant digit to most significant digit. 
+// Radix sort uses counting sort as a subroutine to sort.
+
+// The Radix Sort Algorithm 
+
+// Do the following for each digit I where I varies from the least significant digit to the most significant digit. 
+//     Here we will be sorting the input array using counting sort (or any stable sort) according to the i’th digit.
+
+// Example:
+
+// Original, unsorted list: 170, 45, 75, 90, 802, 24, 2, 66 Sorting by least significant digit (1s place)
+// gives: [*Notice that we keep 802 before 2, because 802 occurred before 2 in the original list, and similarly for pairs 170 & 90 and 45 & 75.] 
+// 170, 90, 802, 2, 24, 45, 75, 66 Sorting by next digit (10s place) gives: [*Notice that 802 again comes before 2 as 802 comes before 2 in the previous list.]
+//     802, 2, 24, 45, 66, 170, 75, 90
+//     Sorting by the most significant digit (100s place) gives: 2, 24, 45, 66, 75, 90, 170, 802
+
 #include <iostream>
 using namespace std;
 
