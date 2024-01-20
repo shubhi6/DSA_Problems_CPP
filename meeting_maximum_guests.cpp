42599639b29b9ecf6e9e371346e2ed1b82c4b687
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-01-20 18:44:00 +0530

    Update meeting_maximum_guests.cpp

--- meeting_maximum_guests.cpp
+++ meeting_maximum_guests.cpp
@@ -1,3 +1,15 @@
+// Consider a big party where a log register for guest's entry and exit times is maintained. Find the time at which there are maximum guests in the party. 
+// Note that entries in register are not in any order.
+// Example : 
+
+// Input: arrl[] = {1, 2, 9, 5, 5}
+//        exit[] = {4, 5, 12, 9, 12}
+// First guest in array arrives at 1 and leaves at 4, 
+// second guest arrives at 2 and leaves at 5, and so on.
+
+// Output: 5
+// There are maximum 3 guests at time 5. 
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -53,3 +65,25 @@ int main()
     findMaxGuests(arrl, exit, n);
     return 0;
 }
+
+
+// Consider the above example. 
+
+//     arr[]  = {1, 2, 10, 5, 5}
+//     dep[]  = {4, 5, 12, 9, 12}
+
+// Below are all events sorted by time.  Note that in sorting, if two
+// events have same time, then arrival is preferred over exit.
+//  Time     Event Type         Total Number of Guests Present
+// ------------------------------------------------------------
+//    1        Arrival                  1
+//    2        Arrival                  2
+//    4        Exit                     1
+//    5        Arrival                  2
+//    5        Arrival                  3   
+//    5        Exit                     2
+//    9        Exit                     1
+//    10       Arrival                  2 
+//    12       Exit                     1
+//    12       Exit                     0    
+
