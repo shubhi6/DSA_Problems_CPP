b80106a8be8f5e206ecf807241afa01bcc7f04a4
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-07 19:17:04 +0530

    Update spiral_traversal_mat.cpp

--- spiral_traversal_mat.cpp
+++ spiral_traversal_mat.cpp
@@ -1,21 +1,21 @@
-Given a 2D array, print it in spiral form. See the following examples.
+// Given a 2D array, print it in spiral form. See the following examples.
 
-Examples: 
+// Examples: 
 
-Input:  {{1,    2,   3,   4},
-              {5,    6,   7,   8},
-             {9,   10,  11,  12},
-            {13,  14,  15,  16 }}
-Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 
-Explanation: The output is matrix in spiral format. 
+// Input:  {{1,    2,   3,   4},
+//               {5,    6,   7,   8},
+//              {9,   10,  11,  12},
+//             {13,  14,  15,  16 }}
+// Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 
+// Explanation: The output is matrix in spiral format. 
 
 
-Input: { {1,   2,   3,   4,  5,   6},
-           {7,   8,   9,  10,  11,  12},
-          {13,  14,  15, 16,  17,  18}}
+// Input: { {1,   2,   3,   4,  5,   6},
+//            {7,   8,   9,  10,  11,  12},
+//           {13,  14,  15, 16,  17,  18}}
 
-Output: 1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
-Explanation :The output is matrix in spiral format. 
+// Output: 1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
+// Explanation :The output is matrix in spiral format. 
 
 #include <iostream>
 using namespace std;
