6b8e426d72394aa3ab798c31761a8b19df418860
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-10 19:12:56 +0530

    Update subarray_with_zerosum_hash.cpp

--- subarray_with_zerosum_hash.cpp
+++ subarray_with_zerosum_hash.cpp
@@ -1,3 +1,20 @@
+Given an array of positive and negative numbers, find if there is a subarray (of size at-least one) with 0 sum.
+Examples : 
+Input: {4, 2, -3, 1, 6}
+Output: true 
+Explanation:
+There is a subarray with zero sum from index 1 to 3.
+
+Input: {4, 2, 0, 1, 6}
+Output: true
+
+Explanation :  
+
+The third element is zero. A single element is also a sub-array. 
+
+Input: {-3, 2, 3, 1, 6}
+Output: false
+
 #include <bits/stdc++.h>
 using namespace std;
 
