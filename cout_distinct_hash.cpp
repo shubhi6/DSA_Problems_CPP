c8b7cd9bdd122b03d4bbf2f018d23d34f2d64961
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-10 19:10:43 +0530

    Update cout_distinct_hash.cpp

--- cout_distinct_hash.cpp
+++ cout_distinct_hash.cpp
@@ -1,3 +1,13 @@
+Given an unsorted array, count all distinct elements in it.
+Examples: 
+
+Input :   arr[] = {10, 20, 20, 10, 30, 10}
+Output : 3
+There are three distinct elements 10, 20 and 30.
+
+Input :   arr[] = {10, 20, 20, 10, 20}
+Output : 2
+
 #include <unordered_set>
 #include <iostream>
 using namespace std;
