7a7d2f47d2fba413e03d6016e6d2960785b5c645
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-11 12:12:45 +0530

    Update Longest _subarray_with_equal_ 0sand1s_hash.cpp

--- Longest _subarray_with_equal_ 0sand1s_hash.cpp
+++ Longest _subarray_with_equal_ 0sand1s_hash.cpp
@@ -7,11 +7,11 @@ Input: arr[] = {1, 0, 1, 1, 1, 0, 0}
 Output: 1 to 6 
 (Starting and Ending indexes of output subarray)
 
-Input: arr[] = {1, 1, 1, 1}
-Output: No such subarray
+// Input: arr[] = {1, 1, 1, 1}
+// Output: No such subarray
 
-Input: arr[] = {0, 0, 1, 1, 0}
-Output: 0 to 3 Or 1 to 4
+// Input: arr[] = {0, 0, 1, 1, 0}
+// Output: 0 to 3 Or 1 to 4
 
 #include <bits/stdc++.h>
 using namespace std;
