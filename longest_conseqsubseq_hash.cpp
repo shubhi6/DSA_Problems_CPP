2ee5a66fae795181bd0500b3c52f4a0af25bc56a
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-11 15:04:30 +0530

    Update longest_conseqsubseq_hash.cpp

--- longest_conseqsubseq_hash.cpp
+++ longest_conseqsubseq_hash.cpp
@@ -1,33 +1,43 @@
+Given an array of integers, find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, 
+the consecutive numbers can be in any order. 
+
+Examples:  
+Input: arr[] = {1, 9, 3, 10, 4, 20, 2}
+Output: 4
+Explanation: 
+The subsequence 1, 3, 4, 2 is the longest 
+subsequence of consecutive elements
+
+Input: arr[] = {36, 41, 56, 35, 44, 33, 34, 92, 43, 32, 42}
+Output: 5
+Explanation: 
+The subsequence 36, 35, 33, 34, 32 is the longest 
+subsequence of consecutive elements.
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Returns length of the longest
-// contiguous subsequence
 int findLongestConseqSubseq(int arr[], int n)
 {
     unordered_set<int> S;
     int ans = 0;
 
-    // Hash all the array elements
+   
     for (int i = 0; i < n; i++)
         S.insert(arr[i]);
 
-    // check each possible sequence from
-    // the start then update optimal length
+ 
     for (int i = 0; i < n; i++)
     {
-        // if current element is the starting
-        // element of a sequence
+        
         if (S.find(arr[i] - 1) == S.end())
         {
-            // Then check for next elements
-            // in the sequence
+            
             int j = arr[i];
             while (S.find(j) != S.end())
                 j++;
 
-            // update  optimal length if
-            // this length is more
+           
             ans = max(ans, j - arr[i]);
         }
     }
