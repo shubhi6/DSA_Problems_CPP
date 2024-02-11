91dfa3277fd2bf05a75cb005d0d2fc7071158543
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-11 15:07:22 +0530

    Update count_distinct_window_hash.cpp

--- count_distinct_window_hash.cpp
+++ count_distinct_window_hash.cpp
@@ -1,24 +1,24 @@
-Given an array of size n and an integer k, return the count of distinct numbers in all windows of size k. 
-Example: 
+// Given an array of size n and an integer k, return the count of distinct numbers in all windows of size k. 
+// Example: 
 
-Input: arr[] = {1, 2, 1, 3, 4, 2, 3};
-       k = 4
-Output: 3 4 4 3
+// Input: arr[] = {1, 2, 1, 3, 4, 2, 3};
+//        k = 4
+// Output: 3 4 4 3
 
-Explanation:
-First window is {1, 2, 1, 3}, count of distinct numbers is 3
-Second window is {2, 1, 3, 4} count of distinct numbers is 4
-Third window is {1, 3, 4, 2} count of distinct numbers is 4
-Fourth window is {3, 4, 2, 3} count of distinct numbers is 3
+// Explanation:
+// First window is {1, 2, 1, 3}, count of distinct numbers is 3
+// Second window is {2, 1, 3, 4} count of distinct numbers is 4
+// Third window is {1, 3, 4, 2} count of distinct numbers is 4
+// Fourth window is {3, 4, 2, 3} count of distinct numbers is 3
 
-Input: arr[] = {1, 2, 4, 4};
-       k = 2
-Output: 2 2 1
+// Input: arr[] = {1, 2, 4, 4};
+//        k = 2
+// Output: 2 2 1
 
-Explanation:
-First window is {1, 2}, count of distinct numbers is 2
-First window is {2, 4}, count of distinct numbers is 2
-First window is {4, 4}, count of distinct numbers is 1
+// Explanation:
+// First window is {1, 2}, count of distinct numbers is 2
+// First window is {2, 4}, count of distinct numbers is 2
+// First window is {4, 4}, count of distinct numbers is 1
  
 
 #include <iostream>
