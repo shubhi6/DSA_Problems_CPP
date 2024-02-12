d587342d86dc6cafd1d1bac0c7aa6da20df8a993
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-12 19:42:23 +0530

    Add files via upload

---
+++ string_palindrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string input) {
+    for (int i = 0; i < input.length(); i++) {
+        if(input[i] != input[input.length() - i - 1])
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+    if (isPalindrome(input)) {
+        cout << input << " is a palindrome" << endl;
+    } else {
+        cout << input << " is not a palindrome" << endl;
+    }
+    return 0;
+}
