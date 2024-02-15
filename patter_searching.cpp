270fe034d6467fc1741be97744744bfbd46ae2d9
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-15 19:12:28 +0530

    Update patter_searching.cpp

--- patter_searching.cpp
+++ patter_searching.cpp
@@ -1,3 +1,17 @@
+Given a text txt[0..n-1] and a pattern pat[0..m-1], write a function search(char pat[], char txt[]) that prints all occurrences of pat[] in txt[]. 
+Assume n > m.
+
+Examples:
+
+Input:  txt[] = "THIS IS A TEST TEXT"
+        pat[] = "TEST"
+Output: Pattern found at index 10
+
+Input:  txt[] =  "AABAACAADAABAABA"
+        pat[] =  "AABA"
+Output: Pattern found at index 0
+        Pattern found at index 9
+        Pattern found at index 12
 
 
 #include <iostream>
