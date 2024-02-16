1ab43750ec2cb40e24199333e7e611a685176e1a
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-16 10:31:07 +0530

    Update rabin_karp_string.cpp

--- rabin_karp_string.cpp
+++ rabin_karp_string.cpp
@@ -1,6 +1,27 @@
+Given a text txt[0..n-1] and a pattern pat[0..m-1], write a function search(char pat[], char txt[]) that prints all occurrences of pat[] in txt[]. 
+Assume that n > m.
+
+Examples:
+Input:  txt[] = "THIS IS A TEST TEXT"
+        pat[] = "TEST"
+Output: Pattern found at index 10
+
+Input:  txt[] =  "AABAACAADAABAABA"
+        pat[] =  "AABA"
+        Enter the prime number = 5
+Output: Pattern found at index 0
+        Pattern found at index 9
+        Pattern found at index 12
+
+
+Input:  txt[] = "acfsskgyh"
+        pat[] = "skg"
+        Enter the prime number = 5
+Output: Pattern found at index 4
+
+
 #include<stdio.h>
 #include<string.h>
-
 #define d 256
 
 void search(char pat[], char txt[], int q)
