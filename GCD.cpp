7aa293a9c59d3092beb74c533827f59db1c83848
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-18 15:21:19 +0530

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -2,16 +2,16 @@
 // The GCD (Greatest Common Divisor) of two or more integers is the largest positive integer that divides each of the integers without leaving a remainder.
 // For example, the GCD of 8 and 12 is 4 because 4 is the largest integer that divides both 8 and 12 without leaving a remainder.
 
-EXAMPLES: 
+// EXAMPLES: 
 
-INPUT : Enter Two Number =  6   9
-OUTPUT : GCD of Numbers = 3
-INPUT : Enter Two Number =  7   9
-OUTPUT : GCD of Numbers = 1
-INPUT : Enter Two Number =  5   15
-OUTPUT : GCD of Numbers = 5
-INPUT : Enter Two Number =  8   4
-OUTPUT : GCD of Numbers = 4
+// INPUT : Enter Two Number =  6   9
+// OUTPUT : GCD of Numbers = 3
+// INPUT : Enter Two Number =  7   9
+// OUTPUT : GCD of Numbers = 1
+// INPUT : Enter Two Number =  5   15
+// OUTPUT : GCD of Numbers = 5
+// INPUT : Enter Two Number =  8   4
+// OUTPUT : GCD of Numbers = 4
 	
 #include<iostream>
 #include<math.h>
