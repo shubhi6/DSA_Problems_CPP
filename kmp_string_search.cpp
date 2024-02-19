5963b38cf3a7570ffda918945baec6447b34515c
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-19 19:30:42 +0530

    Update kmp_string_search.cpp

--- kmp_string_search.cpp
+++ kmp_string_search.cpp
@@ -63,11 +63,11 @@ int main() {
 }
 
 
-Examples:
+// Examples:
 
-Input:  txt[] = "THIS IS A TEST TEXT", pat[] = "TEST"
-Output: Pattern found at index 10
+// Input:  txt[] = "THIS IS A TEST TEXT", pat[] = "TEST"
+// Output: Pattern found at index 10
 
-Input:  txt[] =  "AABAACAADAABAABA"
-          pat[] =  "AABA"
-Output: Pattern found at index 0, Pattern found at index 9, Pattern found at index 12
+// Input:  txt[] =  "AABAACAADAABAABA"
+//           pat[] =  "AABA"
+// Output: Pattern found at index 0, Pattern found at index 9, Pattern found at index 12
