77ba46da17d9c03bbeb5b69ea7d447ab8568285d
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-20 19:49:29 +0530

    Add files via upload

---
+++ largest_substring_distinct_char.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubstrDistinctChars(string s) {
+    if (s.length() == 0)
+        return 0;
+    int n = s.length();
+    set<char> st;
+    int len = 1;
+    st.insert(s[0]);
+    int i = 1;
+    int maxLen = 0;
+    while (i < n) {
+        if (s[i] != s[i - 1] && st.find(s[i]) == st.end()) {
+            st.insert(s[i]);
+            len++;
+            i++;
+            if (len > maxLen) {
+                maxLen = len;
+            }
+        } else {
+            if (len == 1) {
+                i++;
+            } else {
+                st.clear();
+                i = i - len + 1;
+                len = 0;
+            }
+        }
+    }
+    return max(maxLen, len);
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+    int len = longestSubstrDistinctChars(str);
+    cout << "The length of the longest non-repeating character substring: " << len;
+    return 0;
+}
+
