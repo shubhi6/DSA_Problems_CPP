67fbfcc860d0697d312c63248a35a7667b572e3f
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-20 19:50:10 +0530

    Update largest_substring_distinct_char.cpp

--- largest_substring_distinct_char.cpp
+++ largest_substring_distinct_char.cpp
@@ -1,3 +1,13 @@
+// Given a string str, find the length of the longest substring without repeating characters. 
+
+// Example:
+
+// For “ABDEFGABEF”, the longest substring are “BDEFGA” and “DEFGAB”, with length 6.
+
+// For “BBBB” the longest substring is “B”, with length 1.
+
+// For “GEEKSFORGEEKS”, there are two longest substrings shown in the below diagrams, with length 7
+
 #include <bits/stdc++.h>
 using namespace std;
 
