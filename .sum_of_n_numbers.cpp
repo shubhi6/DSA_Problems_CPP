0eece13546799d66305e3c34cfd88a690ebc90d1
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-21 20:55:34 +0530

    Update .sum_of_n_numbers.cpp

--- .sum_of_n_numbers.cpp
+++ .sum_of_n_numbers.cpp
@@ -1,9 +1,10 @@
-//sum of n natural numbers
+//sum of n natural numbers.
+
 // EXAMPLE :
 
 // INPUT: 5
 // OUTPUT: 15
-// EXPLANATION: 1+2+3+4+5=15
+// EXPLANATION: 1+2+3+4+5 = 15
     
 // INPUT: 6
 // OUTPUT: 21
