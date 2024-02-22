116b238f4857299233ddea344db0ca529401a453
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-22 19:42:19 +0530

    Add files via upload

---
+++ traversing_alinked_list.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+// Forward declaration of printlist function
+void printlist(node *head);
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    printlist(head);
+    return 0;
+}
+
+// Definition of printlist function
+void printlist(node *head)
+{
+    node *curr=head;
+    while(curr!=NULL)
+    {
+        cout<<(curr->data)<<" ";
+        curr=curr->next;
+    }
+}
+
