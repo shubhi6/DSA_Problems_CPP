8e705b5ecf939617bb0c56afda12f8a81583ef26
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-23 18:05:31 +0530

    Add files via upload

---
+++ recursive_traversal_linkedlist.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlistrec(node *head);
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    printlistrec(head);
+    return 0;
+}
+
+void printlistrec(node *head)
+{
+    if(head != NULL)
+    {
+        cout << head->data << " ";
+        printlistrec(head->next);
+    }
+}
+
