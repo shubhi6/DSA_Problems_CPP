36e2ea340869af4278d6b49b419d7f839a6e4832
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-23 18:11:38 +0530

    Update insert_atthe_begin_linkedlist.cpp

--- insert_atthe_begin_linkedlist.cpp
+++ insert_atthe_begin_linkedlist.cpp
@@ -1,3 +1,8 @@
+// Approach: The new node is always added before the head of the given Linked List. And newly added node becomes the new head of the Linked List. 
+// For example, if the given Linked List is 10->15->20->25 and we add an item 5 at the front, then the Linked List becomes 5->10->15->20->25. 
+// Let us call the function that adds at the front of the list is push().
+// The push() must receive a pointer to the head pointer because the push must change the head pointer to point to the new node 
+
 #include<iostream>
 using namespace std;
 
