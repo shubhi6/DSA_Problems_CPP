6d12aa90d5d947a53bcbb881f9a511080c85b171
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-24 18:38:20 +0530

    Update middle_of_LL.cpp

--- middle_of_LL.cpp
+++ middle_of_LL.cpp
@@ -1,3 +1,7 @@
+// Given a singly linked list, find the middle of the linked list. For example, if the given linked list is 1->2->3->4->5 then the output should be 3. 
+// If there are even nodes, then there would be two middle nodes, we need to print the second middle element. 
+// For example, if the given linked list is 1->2->3->4->5->6 then the output should be 4. 
+
 //.............METHOD 1.......
 
 #include <iostream>
