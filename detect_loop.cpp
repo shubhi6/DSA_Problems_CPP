7e4a55b22adbb9b47fca7e4e5893df76c1aaa99a
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-26 19:38:57 +0530

    Update detect_loop.cpp

--- detect_loop.cpp
+++ detect_loop.cpp
@@ -1,3 +1,16 @@
+//Given a linked list, check if the linked list has a loop or not.
+// The idea is to point the current node of the linked list to a node which is created. Whenever a node’s next is pointing to that node it means loop is there.
+
+// Follow the steps below to solve the problem:
+
+// A temporary node is created. 
+// The next pointer of each node that is traversed is made to point to this temporary node. 
+// This way we are using the next pointer of a node as a flag to indicate whether the node has been traversed or not. 
+// Every node is checked to see if the next is pointing to a temporary node or not.
+// In the case of the first node of the loop, the second time we traverse it this condition will be true, hence we find that loop exists. 
+// If we come across a node that points to null then the loop doesn’t exist.
+
+	
 #include <bits/stdc++.h>
 using namespace std;
 
