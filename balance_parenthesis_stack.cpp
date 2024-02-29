f3518edd623d5cdc24ca02d5392b4ebe179e73a7
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-02-29 19:40:42 +0530

    Update balance_parenthesis_stack.cpp

--- balance_parenthesis_stack.cpp
+++ balance_parenthesis_stack.cpp
@@ -1,3 +1,16 @@
+// Given an expression string exp, write a program to examine whether the pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in the given expression.
+
+// Example: 
+
+// Input: exp = “[()]{}{[()()]()}” 
+// Output: Balanced
+// Explanation: all the brackets are well-formed
+
+// Input: exp = “[(])” 
+// Output: Not Balanced 
+// Explanation: 1 and 4 brackets are not balanced because 
+// there is a closing ‘]’ before the closing ‘(‘
+
 #include <bits/stdc++.h>
 using namespace std;
 
