6f9e781e3db30c4a4438bc1950b0c4c1419e22d4
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-02 17:18:05 +0530

    Update largest_histogram_area_stack.cpp

--- largest_histogram_area_stack.cpp
+++ largest_histogram_area_stack.cpp
@@ -1,3 +1,14 @@
+// Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a 
+// number of contiguous bars whose heights are given in an array. For simplicity, assume that all bars have the same width and the width is 1 unit. 
+
+// Example: 
+
+// Input: histogram = {6, 2, 5, 4, 5, 1, 6}
+// Output: 12
+
+// Input: histogram = {3, 5, 1, 7, 5, 9}
+// Output: 15
+
 #include <bits/stdc++.h>
 using namespace std;
 
