039cbc916c859fe832a2d535960d14de4eb2e538
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-04 14:39:44 +0530

    Update infix_to_postfix.cpp

--- infix_to_postfix.cpp
+++ infix_to_postfix.cpp
@@ -1,3 +1,16 @@
+// Write a program to Convert Infix expression to Postfix.
+
+// Infix expression: The expression of the form a operator b (a + b). When an operator is in-between every pair of operands.
+// Postfix expression: The expression of the form a b operator (ab+). When an operator is followed by every pair of operands.
+
+// Examples:
+
+// Input: A + B * C + D
+// Output: ABC*+D+
+
+// Input: ((A + B) - C * (D / E)) + F
+// Output: AB+CDE/*-F+  
+
 #include <bits/stdc++.h>
 using namespace std;
 
