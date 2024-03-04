5e08aefc762076ea57f3617cc6e090b25cfd08ae
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-04 14:40:16 +0530

    Update evaluation_of_prefix.cpp

--- evaluation_of_prefix.cpp
+++ evaluation_of_prefix.cpp
@@ -1,3 +1,18 @@
+// Algorithm:
+
+// EVALUATE_PREFIX(STRING)
+// Step 1: Put a pointer P at the end of the end
+// Step 2: If character at P is an operand push it to Stack
+// Step 3: If the character at P is an operator pop two 
+//         elements from the Stack. Operate on these elements
+//         according to the operator, and push the result 
+//         back to the Stack
+// Step 4: Decrement P by 1 and go to Step 2 as long as there
+//         are characters left to be scanned in the expression.
+// Step 5: The Result is stored at the top of the Stack, 
+//         return it
+// Step 6: End
+
 #include <bits/stdc++.h>
 using namespace std;
 
