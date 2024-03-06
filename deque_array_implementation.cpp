4ff1faacd5da75b9792fd23dcdd32bed7847b865
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-06 19:22:36 +0530

    Update deque_array_implementation.cpp

--- deque_array_implementation.cpp
+++ deque_array_implementation.cpp
@@ -1,3 +1,19 @@
+Deque or Double Ended Queue is a generalized version of the Queue data structure that allows insert and delete at both ends
+
+Operations on Deque: 
+Mainly the following four basic operations are performed on queue: 
+
+insertFront(): Adds an item at the front of Deque.
+insertRear(): Adds an item at the rear of Deque. 
+deleteFront(): Deletes an item from front of Deque. 
+deleteRear(): Deletes an item from rear of Deque.
+In addition to above operations, following operations are also supported 
+
+getFront(): Gets the front item from queue. 
+getRear(): Gets the last item from queue. 
+isEmpty(): Checks whether Deque is empty or not. 
+isFull(): Checks whether Deque is full or not. 
+
 #include <iostream>
 using namespace std;
 
