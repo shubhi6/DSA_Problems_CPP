71d9a04e8dfa06e36b973a6b2c9eb8effdd496e4
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-07 18:47:26 +0530

    Update LCM.cpp

--- LCM.cpp
+++ LCM.cpp
@@ -1,11 +1,4 @@
-// Calculating LCM of a number
 
-// LCM stands for "Least Common Multiple." 
-// It refers to the smallest positive integer that is divisible by two or more given integers without leaving a remainder.
-// In simpler terms, the LCM is the smallest number that all the given numbers can divide into evenly.
-
-// For example, the LCM of 4 and 6 is 12 because it is the smallest number that both 4 and 6 can divide into evenly.
-// Similarly, the LCM of 3, 4, and 6 is also 12 because it is the smallest number that all three numbers can divide into evenly.
 
 #include<iostream>
 #include<math.h>
@@ -23,6 +16,14 @@ int LCM(int n,int m)
 	return res;
 	
 }
+// Calculating LCM of a number
+
+// LCM stands for "Least Common Multiple." 
+// It refers to the smallest positive integer that is divisible by two or more given integers without leaving a remainder.
+// In simpler terms, the LCM is the smallest number that all the given numbers can divide into evenly.
+
+// For example, the LCM of 4 and 6 is 12 because it is the smallest number that both 4 and 6 can divide into evenly.
+// Similarly, the LCM of 3, 4, and 6 is also 12 because it is the smallest number that all three numbers can divide into evenly.
 
 int main()
 {
