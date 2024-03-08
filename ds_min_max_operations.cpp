2816857eb58f57d6d6b85524d8acbc7a3aabc641
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-08 18:37:35 +0530

    Update ds_min_max_operations.cpp

--- ds_min_max_operations.cpp
+++ ds_min_max_operations.cpp
@@ -1,3 +1,26 @@
+// Problem: Design a Data Structure a SpecialQueue which supports following operations enqueue, deque, getMin() or getMax() where getMin() operation takes O(1) time.
+// Example: 
+ 
+
+// Let the data to be inserted in queue be -
+// 4, 2, 1, 6
+
+// Operation     Queue       Output
+// push(4)         4           -
+// push(2)        4, 2         -
+// push(1)       4, 2, 1       -
+// getMin()      4, 2, 1       1
+// push(6)      4, 2, 1, 6     -
+// pop()         2, 1, 6       4
+// pop()          1, 6         2
+// pop()            6          1
+// getMin()         6          6
+
+// Notice the getMin() function call
+// It returns the minimum element 
+// of all the values present in the queue
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
