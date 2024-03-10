3587c7d6cae0b8afe1dcf28a2ff00c37b19b160f
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-10 10:37:53 +0530

    Update .sum_of_n_numbers.cpp

--- .sum_of_n_numbers.cpp
+++ .sum_of_n_numbers.cpp
@@ -1,15 +1,4 @@
-//Sum of n natural numbers.
 
-// EXAMPLE :
-
-// INPUT: 5
-// OUTPUT: 15
-// EXPLANATION: 1 + 2 + 3 + 4 + 5 = 15
-
-
-// INPUT: 6
-// OUTPUT: 21
-// EXPLANATION: 1 + 2 + 3 + 4 + 5 + 6 = 21
 
 #include<iostream>
 using namespace std;
