95a46b165e1ece21e9f841cdb83781662df7bf5b
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-12 19:49:18 +0530

    Update LRU_cache_LL.cpp

--- LRU_cache_LL.cpp
+++ LRU_cache_LL.cpp
@@ -2,7 +2,8 @@
 //Design a data structure for LRU Cache. It should support the following operations: get and set.
 //
 //get(key) – Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-//set(key, value) – Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+//set(key, value) – Set or insert the value if the key is not already present. When the cache reached its capacity, 
+//it should invalidate the least recently used item before inserting a new item.
 //Examples:
 
 //Let's say we have a LRU cache of capacity 2.
