b878cf94b14da8ed3044245eb03555af9e8b145c
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-17 17:10:13 +0530

    Update EQLZING.cpp

--- EQLZING.cpp
+++ EQLZING.cpp
@@ -1,4 +1,11 @@
-
+// A man has two integers A and B. In one operation he can choose any integer d, and make one of the following two moves :
+//  1. Add d to A and subtract d from B.
+//  2. Add d to B and subtract d from A.
+// He is allowed to make as many operations as he wants. Can he make A and B equal?
+//total test cases taken are 2
+//input         //output
+//3 3              yes
+//1 2              no
 
 
 #include <bits/stdc++.h>
