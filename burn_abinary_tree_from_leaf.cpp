aedf78c5a1334beee6bc0b5bbf2a0e693f5dc35a
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-18 19:25:57 +0530

    Update burn_abinary_tree_from_leaf.cpp

--- burn_abinary_tree_from_leaf.cpp
+++ burn_abinary_tree_from_leaf.cpp
@@ -71,3 +71,87 @@ int main() {
 //                        10
 // Leaf = 8
 // Output : 7
+
+//...............EXPLANATION.................
+
+// Initially 8 is set to fire at 0th sec.
+//             1
+//        /       \
+//       2          3
+//     /  \          \
+//    4    5          6
+//       /   \         \
+//      7     F         9
+//                       \
+//                        10
+// After 1s: 5 is set to fire.
+//             1
+//        /       \
+//       2          3
+//     /  \          \
+//    4    F          6
+//       /   \         \
+//      7     F         9
+//                       \
+//                        10
+// After 2s: 2, 7 are set to fire.
+//             1
+//        /       \
+//       F          3
+//     /  \          \
+//    4    F          6
+//       /   \         \
+//      F     F         9
+//                       \
+//                        10
+// After 3s: 4, 1 are set to fire.
+//             F
+//        /       \
+//       F          3
+//     /  \          \
+//    F    F          6
+//       /   \         \
+//      F     F         9
+//                       \
+//                        10
+// After 4s: 3 is set to fire.
+//             F
+//        /       \
+//       F          F
+//     /  \          \
+//    F    F          6
+//       /   \         \
+//      F     F         9
+//                       \
+//                        10
+// After 5s: 6 is set to fire.
+//             F
+//        /       \
+//       F          F
+//     /  \          \
+//    F    F          F
+//       /   \         \
+//      F     F         9
+//                       \
+//                        10
+// After 6s: 9 is set to fire.
+//             F
+//        /       \
+//       F          F
+//     /  \          \
+//    F    F          F
+//       /   \         \
+//      F     F         F
+//                       \
+//                        10
+// After 7s: 10 is set to fire.
+//             F
+//        /       \
+//       F          F
+//     /  \          \
+//    F    F          F
+//       /   \         \
+//      F     F         F
+//                       \
+//                        F
+//It takes 7s to burn the complete tree. 
