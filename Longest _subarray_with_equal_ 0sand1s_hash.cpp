e91ba12e93b852b6002eeb8d0549333a03f3c6a3
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-21 18:39:11 +0530

    Update Longest _subarray_with_equal_ 0sand1s_hash.cpp

--- Longest _subarray_with_equal_ 0sand1s_hash.cpp
+++ Longest _subarray_with_equal_ 0sand1s_hash.cpp
@@ -1,18 +1,7 @@
 // Given an array containing only 0s and 1s, find the largest subarray which contains equal no of 0s and 1s. 
 // The expected time complexity is O(n). 
 
-// Examples: 
-
-// Input: arr[] = {1, 0, 1, 1, 1, 0, 0}
-// Output: 1 to 6 
-// (Starting and Ending indexes of output subarray)
-
-// Input: arr[] = {1, 1, 1, 1}
-// Output: No such subarray
-
-// Input: arr[] = {0, 0, 1, 1, 0}
-// Output: 0 to 3 Or 1 to 4
-
+//
 #include <bits/stdc++.h>
 using namespace std;
 
