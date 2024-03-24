27a6fe81f50b9ee04e321d249102e2c8062cec97
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-24 17:36:36 +0530

    Update binary_tree_traversal.cpp

--- binary_tree_traversal.cpp
+++ binary_tree_traversal.cpp
@@ -1,11 +1,6 @@
 //................Binary Tree Traversals..................
 
-// Contrust the Tree
-    //      1
-    //     /  \
-    //    2    3
-    //   / \    
-    //  4   5
+
 // Unlike linear data structures (Array, Linked List, Queues, Stacks, etc.), 
 // which have only one logical way to traverse them, trees can be traversed in different ways. 
 // Following are the generally used ways for traversing trees:
