108e4b971480cfdb0887ec8354aa902d78fe8c40
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-27 20:23:53 +0530

    Update .sum_of_n_numbers.cpp

--- .sum_of_n_numbers.cpp
+++ .sum_of_n_numbers.cpp
@@ -6,6 +6,10 @@
 // OUTPUT: 15
 // EXPLANATION: 1 + 2 + 3 + 4 + 5 = 15
 
+// INPUT: 6
+// OUTPUT: 21
+// EXPLANATION: 1 + 2 + 3 + 4 + 5 + 6 = 21
+
 #include<iostream>
 using namespace std;
 int main()
