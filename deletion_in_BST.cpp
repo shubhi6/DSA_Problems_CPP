1733e6bfb6a3099d8d8bea0da920641422323658
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-28 19:42:42 +0530

    Add files via upload

---
+++ deletion_in_BST.cpp
@@ -0,0 +1,107 @@
+
+    /* Let us create following BST
+          50
+        /     \
+       30     70
+      /  \   / \
+     20  40 60  80 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int key;
+    struct node *left, *right;
+};
+
+struct node *newNode(int item)
+{
+    node *temp = new node;
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout<<root->key<<" ";
+        inorder(root->right);
+    }
+}
+
+struct node* insert(struct node* node, int key)
+{
+    if (node == NULL) 
+        return newNode(key);
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
+    return node;
+}
+
+struct node * minValueNode(struct node* node)
+{
+    struct node* current = node;
+    while (current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+struct node* deleteNode(struct node* root, int key)
+{
+    if (root == NULL) return root;
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+    else
+    {
+        if (root->left == NULL)
+        {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        struct node* temp = minValueNode(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+int main()
+{
+    struct node *root = NULL;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    inorder(root);
+
+    root = deleteNode(root, 20);
+    inorder(root);
+
+    root = deleteNode(root, 30);
+    inorder(root);
+
+    root = deleteNode(root, 50); 
+    inorder(root);
+
+    return 0;
+}
+
