16b1a2d578e4932520caa6ef4372dd4cfdce5073
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-29 18:53:47 +0530

    Update ceil_in_BST.cpp

--- ceil_in_BST.cpp
+++ ceil_in_BST.cpp
@@ -92,17 +92,17 @@ int main()
 
 
 
-Algorithm:  
+// Algorithm:  
 
-Imagine we are moving down the tree, and assume we are root node. 
-The comparison yields three possibilities,
+// Imagine we are moving down the tree, and assume we are root node. 
+// The comparison yields three possibilities,
 
-A) Root data is equal to key. We are done, root data is ceil value.
+// A) Root data is equal to key. We are done, root data is ceil value.
 
-B) Root data < key value, certainly the ceil value can't be in left subtree. 
-   Proceed to search on right subtree as reduced problem instance.
+// B) Root data < key value, certainly the ceil value can't be in left subtree. 
+//    Proceed to search on right subtree as reduced problem instance.
 
-C) Root data > key value, the ceil value may be in left subtree. 
-   We may find a node with is larger data than key value in left subtree, 
-   if not the root itself will be ceil node.
+// C) Root data > key value, the ceil value may be in left subtree. 
+//    We may find a node with is larger data than key value in left subtree, 
+//    if not the root itself will be ceil node.
 
