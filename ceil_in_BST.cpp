bf88919f6269d85349aacf8edc8f8227dec1d5b5
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-29 18:51:42 +0530

    Update ceil_in_BST.cpp

--- ceil_in_BST.cpp
+++ ceil_in_BST.cpp
@@ -1,3 +1,21 @@
+Ceil in BST
+
+Given a binary tree and a key(node) value, find the ceil value for that particular key value.
+Ceil Value Node: Node with the smallest data larger than or equal to the key value.
+
+For example, Let's consider the Binary Tree below - 
+
+          8
+        /   \    
+      4      12
+    /  \    /  \
+   2    6  10   14
+
+Key: 11   Ceil: 12
+Key: 1    Ceil: 2
+Key: 6    Ceil: 6
+Key: 15   Ceil: -1
+
 #include <bits/stdc++.h> 
 using namespace std; 
 
