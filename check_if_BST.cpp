95ee7ef52820f19dd4baee17e194177965dab3e1
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-30 19:59:30 +0530

    Update check_if_BST.cpp

--- check_if_BST.cpp
+++ check_if_BST.cpp
@@ -1,3 +1,11 @@
+Check for BST
+
+A binary search tree (BST) is a node based binary tree data structure which has the following properties. 
+
+* The left subtree of a node contains only nodes with keys less than the node's key.
+* The right subtree of a node contains only nodes with keys greater than the node's key.
+* Both the left and right subtrees must also be binary search trees.
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -53,3 +61,6 @@ int main()
     return 0; 
 }
 
+Output:  
+
+Not a BST
