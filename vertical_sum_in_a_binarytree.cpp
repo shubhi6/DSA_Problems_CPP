9c5badfb7028d1052225eff4bd05607b9c568728
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-03-31 10:56:32 +0530

    Update vertical_sum_in_a_binarytree.cpp

--- vertical_sum_in_a_binarytree.cpp
+++ vertical_sum_in_a_binarytree.cpp
@@ -1,3 +1,23 @@
+//................. Vertical Sum in a Binary Tree...................
+
+// Given a Binary Tree, find the vertical sum of the nodes that are in the same vertical line. Print all sums through different vertical lines.
+// Examples: 
+ 
+
+//       1
+//     /   \
+//   2      3
+//  / \    / \
+// 4   5  6   7
+
+// The tree has 5 vertical lines
+// Vertical-Line-1 has only one node 4 => vertical sum is 4 
+// Vertical-Line-2: has only one node 2=> vertical sum is 2 
+// Vertical-Line-3: has three nodes: 1,5,6 => vertical sum is 1+5+6 = 12 
+// Vertical-Line-4: has only one node 3 => vertical sum is 3 
+// Vertical-Line-5: has only one node 7 => vertical sum is 7
+So expected output is 4, 2, 12, 3 and 7 
+
 #include<bits/stdc++.h>
 using namespace std;
 
