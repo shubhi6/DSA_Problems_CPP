26f0e06a1e7cb8935503b484b1beec7f831fec4b
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-02 19:26:49 +0530

    Update operatios_of_heap_data_structure.cpp

--- operatios_of_heap_data_structure.cpp
+++ operatios_of_heap_data_structure.cpp
@@ -3,13 +3,18 @@ Operations of Heap Data Structure
 Operations on Min Heap:
 1) getMini(): It returns the root element of Min Heap. Time Complexity of this operation is O(1).
 
-2) extractMin(): Removes the minimum element from MinHeap. Time Complexity of this Operation is O(Logn) as this operation needs to maintain the heap property (by calling heapify()) after removing root.
+2) extractMin(): Removes the minimum element from MinHeap. Time Complexity of this Operation is O(Logn) as this operation needs to maintain the heap property 
+                 (by calling heapify()) after removing root.
 
-3) decreaseKey(): Decreases value of key. The time complexity of this operation is O(Logn). If the decreases key value of a node is greater than the parent of the node, then we don’t need to do anything. Otherwise, we need to traverse up to fix the violated heap property.
+3) decreaseKey(): Decreases value of key. The time complexity of this operation is O(Logn). 
+	         If the decreases key value of a node is greater than the parent of the node, then we don’t need to do anything.
+	         Otherwise, we need to traverse up to fix the violated heap property.
 
-4) insert(): Inserting a new key takes O(Logn) time. We add a new key at the end of the tree. IF new key is greater than its parent, then we don’t need to do anything. Otherwise, we need to traverse up to fix the violated heap property.
+4) insert(): Inserting a new key takes O(Logn) time. We add a new key at the end of the tree. 
+	     IF new key is greater than its parent, then we don’t need to do anything. Otherwise, we need to traverse up to fix the violated heap property.
 
-5) delete(): Deleting a key also takes O(Logn) time. We replace the key to be deleted with minum infinite by calling decreaseKey(). After decreaseKey(), the minus infinite value must reach root, so we call extractMin() to remove the key.
+5) delete(): Deleting a key also takes O(Logn) time. We replace the key to be deleted with minum infinite by calling decreaseKey().
+             After decreaseKey(), the minus infinite value must reach root, so we call extractMin() to remove the key.
 
 #include<iostream>
 #include<climits>
