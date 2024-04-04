dd08d6556c07e998250bb1735fb98e39d3e1be15
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-04 13:30:18 +0530

    Update buy_max_items_with_given_sum.cpp

--- buy_max_items_with_given_sum.cpp
+++ buy_max_items_with_given_sum.cpp
@@ -1,19 +1,19 @@
 //...................Buy Maximum Items with Given Sum.....................
 
-Given an array arr[] consisting of the cost of toys and an integer K depicting the amount of money available to purchase toys.
-The task is to find the maximum number of toys one can buy with the amount K.
-Note: One can buy only 1 quantity of a particular toy.
+// Given an array arr[] consisting of the cost of toys and an integer K depicting the amount of money available to purchase toys.
+// The task is to find the maximum number of toys one can buy with the amount K.
+// Note: One can buy only 1 quantity of a particular toy.
 
-Examples: 
+// Examples: 
 
-Input: arr[] = {1, 12, 5, 111, 200, 1000, 10, 9, 12, 15}, K = 50 
-Output: 6 
-Toys with amount 1, 5, 9, 10, 12, and 12 
-can be purchased resulting in a total amount of 49. 
-Hence, the maximum number of toys are 6.
+// Input: arr[] = {1, 12, 5, 111, 200, 1000, 10, 9, 12, 15}, K = 50 
+// Output: 6 
+// Toys with amount 1, 5, 9, 10, 12, and 12 
+// can be purchased resulting in a total amount of 49. 
+// Hence, the maximum number of toys are 6.
 
-Input: arr[] = {1, 12, 5, 111, 200, 1000, 10}, K = 50 
-Output: 4
+// Input: arr[] = {1, 12, 5, 111, 200, 1000, 10}, K = 50 
+// Output: 4
 
 #include <bits/stdc++.h>
 using namespace std;
