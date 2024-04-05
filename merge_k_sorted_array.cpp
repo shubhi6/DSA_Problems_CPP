feb1ebcb655f270a56667a1197e86ec7c65e8130
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-05 19:57:38 +0530

    Update merge_k_sorted_array.cpp

--- merge_k_sorted_array.cpp
+++ merge_k_sorted_array.cpp
@@ -1,18 +1,18 @@
 //.....................Merge K Sorted Arrays........................
 
-Given k sorted arrays of possibly different sizes, merge them and print the sorted output.
-Examples: 
+// Given k sorted arrays of possibly different sizes, merge them and print the sorted output.
+// Examples: 
 
-Input: k = 3 
-      arr[][] = { {1, 3},
-                  {2, 4, 6},
-                  {0, 9, 10, 11}} ;
-Output: 0 1 2 3 4 6 9 10 11 
+// Input: k = 3 
+//       arr[][] = { {1, 3},
+//                   {2, 4, 6},
+//                   {0, 9, 10, 11}} ;
+// Output: 0 1 2 3 4 6 9 10 11 
 
-Input: k = 2
-      arr[][] = { {1, 3, 20},
-                  {2, 4, 6}} ;
-Output: 1 2 3 4 6 20 
+// Input: k = 2
+//       arr[][] = { {1, 3, 20},
+//                   {2, 4, 6}} ;
+// Output: 1 2 3 4 6 20 
 
 
 #include <bits/stdc++.h>
