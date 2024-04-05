fe255fcc2373e8cc4297615336c97cfdc6ea93ce
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-05 19:56:45 +0530

    Update k_closest_elements.cpp

--- k_closest_elements.cpp
+++ k_closest_elements.cpp
@@ -1,3 +1,15 @@
+.....................K Closest Elements.........................
+
+Given a sorted array arr[] and a value X, find the k closest elements to X in arr[]. 
+
+Examples: 
+
+Input: K = 4, X = 35
+       arr[] = {12, 16, 22, 30, 35, 39, 42, 
+               45, 48, 50, 53, 55, 56}
+Output: 30 39 42 45
+
+
 #include<stdio.h> 
 
 int findCrossOver(int arr[], int low, int high, int x) 
