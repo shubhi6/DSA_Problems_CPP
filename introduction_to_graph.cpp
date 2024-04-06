11da98affb181bcddcc0c3261ee91ea718030129
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-06 11:46:58 +0530

    Update introduction_to_graph.cpp

--- introduction_to_graph.cpp
+++ introduction_to_graph.cpp
@@ -1,3 +1,14 @@
+Introduction to Graphs
+
+A Graph is a data structure that consists of the following two components:
+A finite set of vertices also called nodes.
+A finite set of ordered pair of the form (u, v) called as edge. The pair is ordered because (u, v) is not the same as (v, u) in case of a directed graph(digraph). The pair of the form (u, v) indicates that there is an edge from vertex u to vertex v. The edges may contain weight/value/cost.
+
+
+Graphs are used to represent many real-life applications:
+Graphs are used to represent networks. The networks may include paths in a city or telephone network or circuit network. For example Google GPS
+Graphs are also used in social networks like linkedIn, Facebook. For example, in Facebook, each person is represented with a vertex(or node). Each node is a structure and contains information like person id, name, gender and locale.
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -34,3 +45,17 @@ int main()
     return 0;
 }
 
+ Adjacency list of vertex 0
+ head -> 1-> 4
+
+ Adjacency list of vertex 1
+ head -> 0-> 2-> 3-> 4
+
+ Adjacency list of vertex 2
+ head -> 1-> 3
+
+ Adjacency list of vertex 3
+ head -> 1-> 2-> 4
+
+ Adjacency list of vertex 4
+ head -> 0-> 1-> 3
