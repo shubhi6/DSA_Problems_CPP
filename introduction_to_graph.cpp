4726c9a1df68726b2d4214c87412485473166c9d
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-06 11:48:36 +0530

    Update introduction_to_graph.cpp

--- introduction_to_graph.cpp
+++ introduction_to_graph.cpp
@@ -1,13 +1,16 @@
-................Introduction to Graphs........................
+// ................Introduction to Graphs........................
 
-A Graph is a data structure that consists of the following two components:
-A finite set of vertices also called nodes.
-A finite set of ordered pair of the form (u, v) called as edge. The pair is ordered because (u, v) is not the same as (v, u) in case of a directed graph(digraph). The pair of the form (u, v) indicates that there is an edge from vertex u to vertex v. The edges may contain weight/value/cost.
+// A Graph is a data structure that consists of the following two components:
+
+// 1. A finite set of vertices also called nodes.
+// 2. A finite set of ordered pair of the form (u, v) called as edge.The pair is ordered because (u, v) is not the same as (v, u) in case of a directed graph(digraph).
+//   The pair of the form (u, v) indicates that there is an edge from vertex u to vertex v. The edges may contain weight/value/cost.
 
 
-Graphs are used to represent many real-life applications:
-Graphs are used to represent networks. The networks may include paths in a city or telephone network or circuit network. For example Google GPS
-Graphs are also used in social networks like linkedIn, Facebook. For example, in Facebook, each person is represented with a vertex(or node). Each node is a structure and contains information like person id, name, gender and locale.
+// Graphs are used to represent many real-life applications:
+// * Graphs are used to represent networks. The networks may include paths in a city or telephone network or circuit network. For example Google GPS
+// * Graphs are also used in social networks like linkedIn, Facebook. For example, in Facebook, each person is represented with a vertex(or node).
+//   Each node is a structure and contains information like person id, name, gender and locale.
 
 #include<bits/stdc++.h>
 using namespace std;
