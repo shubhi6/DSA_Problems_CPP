25e3adb7826e4592d8d2b6455e5e77de033080e1
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-11 19:35:48 +0530

    Update kosaraju's_algorithm_strongly_connected_components.cpp

--- kosaraju's_algorithm_strongly_connected_components.cpp
+++ kosaraju's_algorithm_strongly_connected_components.cpp
@@ -1,3 +1,16 @@
+......................Kosaraju's algorithm............................
+    
+Following is detailed Kosaraju's algorithm.
+
+1.Create an empty stack 'S' and do DFS traversal of a graph. In DFS traversal, after calling recursive DFS for adjacent vertices of a vertex, 
+push the vertex to stack. In the above graph, if we start DFS from vertex 0, we get vertices in the stack as 1, 2, 4, 3, 0.
+    
+2.Reverse directions of all arcs to obtain the transpose graph.
+    
+3.One by one pop a vertex from S while S is not empty. Let the popped vertex be 'v'. Take v as source and do DFS (call DFSUtil(v)).
+The DFS starting from v prints strongly connected component of v. In the above example, we process vertices in order 0, 3, 4, 2, 1 (One by one popped from stack).
+
+
 #include <iostream>
 #include <list>
 #include <stack>
