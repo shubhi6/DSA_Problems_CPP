c1dd220f78c5c9b789bddceb5fdfc7bbb28ef9dd
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-04-13 19:01:25 +0530

    Update job_sequence_problem_greedy_approach.cpp

--- job_sequence_problem_greedy_approach.cpp
+++ job_sequence_problem_greedy_approach.cpp
@@ -1,39 +1,39 @@
-..................................Job Sequencing Problem | Greedy Approach.........................
+// ..................................Job Sequencing Problem | Greedy Approach.........................
 
-Problem: Given an array of jobs where every job has a deadline and associated profit if the job is finished before the deadline. 
-It is also given that every job takes a single unit of time, so the minimum possible deadline for any job is 1.
-How to maximize total profit if only one job can be scheduled at a time.
+// Problem: Given an array of jobs where every job has a deadline and associated profit if the job is finished before the deadline. 
+// It is also given that every job takes a single unit of time, so the minimum possible deadline for any job is 1.
+// How to maximize total profit if only one job can be scheduled at a time.
 
-Examples:
-Input: Four Jobs with following deadlines and profits
-  JobID    Deadline      Profit
-    a        4            20   
-    b        1            10
-    c        1            40  
-    d        1            30
-Output: Following is maximum profit sequence of jobs
-        c, a   
+// Examples:
+// Input: Four Jobs with following deadlines and profits
+//   JobID    Deadline      Profit
+//     a        4            20   
+//     b        1            10
+//     c        1            40  
+//     d        1            30
+// Output: Following is maximum profit sequence of jobs
+//         c, a   
 
-Input: Five Jobs with following deadlines and profits
-   JobID     Deadline     Profit
-     a         2           100
-     b         1           19
-     c         2           27
-     d         1           25
-     e         3           15
-Output: Following is maximum profit sequence of jobs
-        c, a, e
-
-
+// Input: Five Jobs with following deadlines and profits
+//    JobID     Deadline     Profit
+//      a         2           100
+//      b         1           19
+//      c         2           27
+//      d         1           25
+//      e         3           15
+// Output: Following is maximum profit sequence of jobs
+//         c, a, e
 
 
-This is a standard Greedy Algorithm problem. Following is the greedy algorithm to solve the above problem:
-1) Sort all jobs in decreasing order of profit.
-2) Initialize the result sequence as the first job in sorted jobs.
-3) Do following for remaining n-1 jobs
-.......a) If the current job can fit in the current result sequence 
-          without missing the deadline, add the current job to the result.
-          Else ignore the current job.
+
+
+// This is a standard Greedy Algorithm problem. Following is the greedy algorithm to solve the above problem:
+// 1) Sort all jobs in decreasing order of profit.
+// 2) Initialize the result sequence as the first job in sorted jobs.
+// 3) Do following for remaining n-1 jobs
+// .......a) If the current job can fit in the current result sequence 
+//           without missing the deadline, add the current job to the result.
+//           Else ignore the current job.
 
 
 
