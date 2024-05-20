3bdb010590841ad21060a1f9bf70fb8f1cad1bd5
Shubhi Tiwari <155247726+shubhi6@users.noreply.github.com>
2024-05-20 12:06:20 +0530

    Add files via upload

---
+++ Majority Element II.cpp
@@ -0,0 +1,68 @@
+//Majority Element II
+
+//Given an integer array of size n, find all elements that appear more than ? n/3 ? times.
+
+//Example 1:
+
+//Input: nums = [3,2,3]
+//Output: [3]
+//Example 2:
+
+//Input: nums = [1]
+//Output: [1]
+//Example 3:
+
+//Input: nums = [1,2]
+//Output: [1,2]
+
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> result;
+        int n = nums.size();
+        
+        if (n == 0) return result;
+        
+        int candidate1 = nums[0], count1 = 0;
+        int candidate2 = nums[0], count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+        
+        if (count1 > n / 3) {
+            result.push_back(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.push_back(candidate2);
+        }
+        
+        return result;
+    }
+};
+
